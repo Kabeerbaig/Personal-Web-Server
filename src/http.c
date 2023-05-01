2c39316da0c66afb653840f9fd21f02ac50337d3
John Tomas <j.johntomas@gmail.com>
2023-05-01 13:49:56 -0400

    only fallback when enabled

--- src/http.c
+++ src/http.c
@@ -387,35 +387,44 @@ handle_static_asset(struct http_transaction *ta, char *basedir)
     }
 
     snprintf(fname, sizeof fname, "%s%s", basedir, req_path);
-    char *alternative_path = realpath(fname, NULL);
 
-    // check to see if the requested path has "/"
-    // also check if the requested file is not found then we update fname to /index.html
-    if (strcmp(req_path, "/") == 0)
+    if (html5_fallback)
     {
-        snprintf(fname, sizeof fname, "%s/index.html", basedir);
-    }
+        char *alternative_path = realpath(fname, NULL);
 
-    // If file cannot be found, return 200.html
-    else if (alternative_path == NULL)
-    {
-        snprintf(fname, sizeof fname, "%s/200.html", basedir);
-    }
-    // Checks if file path starts with resolved base directory
-    // Return 404 Error if it doesnt start with it
-    else if (alternative_path != NULL)
-    {
-
-        if (strstr(alternative_path, ".") != NULL)
+        // check to see if the requested path has "/"
+        // also check if the requested file is not found then we update fname to /index.html
+        if (strcasecmp(req_path, "/") == 0)
         {
-            snprintf(fname, sizeof fname, "%s", alternative_path);
-        }
-        else
-        {
-            snprintf(fname, sizeof fname, "%s.html", alternative_path);
+            snprintf(fname, sizeof fname, "%s/index.html", basedir);
         }
 
-        free(alternative_path);
+        // If file cannot be found, return 200.html
+        else if (alternative_path == NULL)
+        {
+            // alternative_path = realpath("/200.html", NULL);
+            // if (alternative_path == NULL)
+            // {
+            //     return send_not_found(ta);
+            // }
+            snprintf(fname, sizeof fname, "%s/200.html", basedir);
+        }
+        // Checks if file path starts with resolved base directory
+        // Return 404 Error if it doesnt start with it
+        else if (alternative_path != NULL)
+        {
+
+            if (strstr(alternative_path, ".") != NULL)
+            {
+                snprintf(fname, sizeof fname, "%s", alternative_path);
+            }
+            else
+            {
+                snprintf(fname, sizeof fname, "%s.html", alternative_path);
+            }
+
+            free(alternative_path);
+        }
     }
 
     if (access(fname, R_OK))
